99e1f5ed7ab92f6e5d38f3ce3e1de677154432f1
Yihyun <hihyun16@gmail.com>
2021-07-15 15:27:18 +0900

    feat : Implement test routing - test routing 구현 (POST /api/user/test)

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import logger from 'morgan';
 import config from './config';
 import { createConnection } from 'typeorm';
 import connectionOptions from './ormconfig';
-// import indexRouter from './api';
+import indexRouter from './api';
 
 const app = express();
 const PORT = config.port || 3000;
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/api', indexRouter);
+app.use('/api', indexRouter);
 
 app.use((req, res, next) => {
     next(createError(404));
