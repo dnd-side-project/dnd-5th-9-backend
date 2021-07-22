2b56594760b7a36475572c4ab5dacfa59b665d1c
YIHYUN HA <59721541+hyh1016@users.noreply.github.com>
2021-07-22 14:53:50 +0900

    [Feature/db] User-Meeting Relation 추가, 오타 수정 (#17)
    * fix: Fix typo of types/index
    - MeetingMemberScehdule -> MeetingMemberSchedule
    
    * feat: add Many-To-Many relation to User, Meeting table
    - User와 Meeting이 다대다 관계를 갖도록 수정
    
    * style: set order of import
    - TypeORM method 뒤에 entity가 오도록 고정

--- src/types/index.ts
+++ src/types/index.ts
@@ -43,7 +43,7 @@ export interface MeetingMember {
     lng?: number;
 }
 
-export interface MeetingMemberScehdule {
+export interface MeetingMemberSchedule {
     id?: number;
     meetingMemberId?: number;
     startDate?: Date;
