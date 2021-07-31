78789fa3bc629b15db2f2919d3508fcdd5272c38
Yihyun <hihyun16@gmail.com>
2021-07-31 14:36:03 +0900

    feat: implement checkUser API - 사용자 이메일을 받아 비밀번호 변경 메일을 전송하는 checkUser API 구현

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,12 +6,13 @@ import {
     Patch,
     Param,
     Delete,
+    NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiTags } from '@nestjs/swagger';
-import sendMail from 'src/lib/sendMail';
+import { CheckUserDto } from './dto/check-user.dto';
 
 @ApiTags('users')
 @Controller('users')
@@ -21,6 +22,12 @@ export class UsersController {
     @Post()
     create(@Body() createUserDto: CreateUserDto) {
         return this.userService.create(createUserDto);
+    }
+
+    @Post('check')
+    async checkUser(@Body() checkUserDto: CheckUserDto) {
+        const result = await this.userService.check(checkUserDto);
+        if (!result) throw new NotFoundException();
     }
 
     @Get()
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import User from '../entities/User';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([User])],
     controllers: [UsersController],
     providers: [UsersService],
 })
