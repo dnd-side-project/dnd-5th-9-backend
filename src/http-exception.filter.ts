67b492127309160176de98e38b31e6a36e0b844c
seoha <devsh4030@gmail.com>
2021-08-01 01:08:15 +0900

    Update: add typeOrm loggering, http-exception

---
+++ src/http-exception.filter.ts
@@ -0,0 +1,33 @@
+import {
+    ExceptionFilter,
+    Catch,
+    ArgumentsHost,
+    HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const status = exception.getStatus();
+        const err = exception.getResponse() as
+            | string
+            | { error: string; statusCode: 400; message: string[] };
+
+        if (typeof err !== 'string' && err.error === 'Bad Request') {
+            return response.status(status).json({
+                result: false,
+                code: status,
+                data: { message: err.message },
+            });
+        }
+
+        response.status(status).json({
+            result: false,
+            code: status,
+            data: { message: err['message'] },
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 declare const module: any;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalFilters(new HttpExceptionFilter());
 
     app.useGlobalPipes(
         new ValidationPipe({
