ac56ce34454f5d1f306c466c6678121f59055e00
Yihyun <hihyun16@gmail.com>
2021-08-01 00:30:46 +0900

    fix: Resolve import entity error - module에 entity를 import하지 않아 생긴 에러 해결

--- src/meetings/meetings.module.ts
+++ src/meetings/meetings.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MeetingsService } from './meetings.service';
 import { MeetingsController } from './meetings.controller';
+import MeetingPlaces from '../entities/MeetingPlaces';
+import MeetingMembers from '../entities/MeetingMembers';
 
 @Module({
-  controllers: [MeetingsController],
-  providers: [MeetingsService]
+    imports: [TypeOrmModule.forFeature([MeetingMembers, MeetingPlaces])],
+    controllers: [MeetingsController],
+    providers: [MeetingsService],
 })
 export class MeetingsModule {}
