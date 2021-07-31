b1f7f8e1ab708f9fbe280102952f6ae75bf68819
Yihyun <hihyun16@gmail.com>
2021-07-31 23:59:31 +0900

    feat: Implement createPlace API - 특정 멤버에 대한 미팅 장소 정보를 생성하는 API 구현

---
+++ src/meetings/dto/create-meeting-place.dto.ts
@@ -0,0 +1,5 @@
+export class CreateMeetingPlaceDto {
+    memberId: number;
+    lat: number;
+    lng: number;
+}
--- src/meetings/meetings.controller.ts
+++ src/meetings/meetings.controller.ts
@@ -1,34 +1,51 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { CreateMeetingPlaceDto } from './dto/create-meeting-place.dto';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+} from '@nestjs/common';
 import { MeetingsService } from './meetings.service';
 import { CreateMeetingDto } from './dto/create-meeting.dto';
 import { UpdateMeetingDto } from './dto/update-meeting.dto';
 
 @Controller('meetings')
 export class MeetingsController {
-  constructor(private readonly meetingsService: MeetingsService) {}
+    constructor(private readonly meetingsService: MeetingsService) {}
 
-  @Post()
-  create(@Body() createMeetingDto: CreateMeetingDto) {
-    return this.meetingsService.create(createMeetingDto);
-  }
+    @Post()
+    create(@Body() createMeetingDto: CreateMeetingDto) {
+        return this.meetingsService.create(createMeetingDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.meetingsService.findAll();
-  }
+    @Post('place')
+    createPlace(@Body() createMeetingPlaceDto: CreateMeetingPlaceDto) {
+        return this.meetingsService.createPlace(createMeetingPlaceDto);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.meetingsService.findOne(+id);
-  }
+    @Get()
+    findAll() {
+        return this.meetingsService.findAll();
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMeetingDto: UpdateMeetingDto) {
-    return this.meetingsService.update(+id, updateMeetingDto);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.meetingsService.findOne(+id);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.meetingsService.remove(+id);
-  }
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body() updateMeetingDto: UpdateMeetingDto
+    ) {
+        return this.meetingsService.update(+id, updateMeetingDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.meetingsService.remove(+id);
+    }
 }
