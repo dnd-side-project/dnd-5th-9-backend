5f43c0e67d0171c4e4f4729c346a68932b4bdc93
Yihyun <hihyun16@gmail.com>
2021-08-01 00:32:43 +0900

    fix: Add class validator to CreateMeetingPlaceDto - 장소 생성 DTO에 class validator가 빠진 문제 해결

--- src/meetings/dto/create-meeting-place.dto.ts
+++ src/meetings/dto/create-meeting-place.dto.ts
@@ -1,5 +1,12 @@
+import { IsNumber } from 'class-validator';
+
 export class CreateMeetingPlaceDto {
+    @IsNumber()
     memberId: number;
+
+    @IsNumber()
     lat: number;
+
+    @IsNumber()
     lng: number;
 }
