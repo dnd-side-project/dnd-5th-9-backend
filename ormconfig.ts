79d0f58b3a5412bdc5b6768dc997f0e9efa6a6d2
summer <72724165+devseoha@users.noreply.github.com>
2021-08-01 14:05:09 +0900

    Revert "feature: create meeting" (#51)
    This reverts commit 2a246f4dcde67f3fc82dc98268a3d9a1a8cd13c8.

--- ormconfig.ts
+++ ormconfig.ts
@@ -31,7 +31,7 @@ const config: TypeOrmModuleOptions = {
     cli: { migrationsDir: 'src/migrations' },
     autoLoadEntities: true,
     charset: 'utf8mb4',
-    synchronize: false,
+    synchronize: true,
     logging: true,
     keepConnectionAlive: true,
 };
