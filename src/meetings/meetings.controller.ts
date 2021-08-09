9f754a9db0340d3f082d6d05b5bd7405c2136563
Yihyun <hihyun16@gmail.com>
2021-08-10 00:31:16 +0900

    refactor: Refactoring controller - 반환 형식 통일, 가드 적용, 일부 API의 파라미터 수정

--- src/meetings/meetings.controller.ts
+++ src/meetings/meetings.controller.ts
@@ -14,30 +14,34 @@ import { CreateMeetingDto } from './dto/create-meeting.dto';
 import { CreateMeetingPlaceDto } from './dto/create-meeting-place.dto';
 import { CreateMeetingMemberDto } from './dto/create-meeting-member.dto';
 import { UpdateMeetingDto } from './dto/update-meeting.dto';
+import { UpdateMeetingScheduleDto } from './dto/update-meeting-schedule.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { OptionalJwtAuthGuard } from '../auth/optional-jwt-auth.guard';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('meetings')
 @Controller('meetings')
 export class MeetingsController {
     constructor(private readonly meetingsService: MeetingsService) {}
 
-    @ApiBearerAuth()
-    @ApiOperation({ summary: '모임생성하기' })
-    @UseGuards(JwtAuthGuard)
+    @ApiOperation({ summary: '미팅 생성' })
+    @ApiBody({ type: CreateMeetingDto })
+    @UseGuards(OptionalJwtAuthGuard)
     @Post()
     create(@Req() req, @Body() createMeetingDto: CreateMeetingDto) {
         return this.meetingsService.create(req.user.id, createMeetingDto);
     }
 
-    @ApiOperation({ summary: '' })
+    @ApiOperation({ summary: '미팅 장소 추가' })
+    @ApiBody({ type: CreateMeetingPlaceDto })
     @Post('place')
     createPlace(@Body() createMeetingPlaceDto: CreateMeetingPlaceDto) {
         return this.meetingsService.createPlace(createMeetingPlaceDto);
     }
 
-    @ApiOperation({ summary: '미팅멤버추가' })
-    @UseGuards(JwtAuthGuard)
+    @ApiOperation({ summary: '미팅 멤버 추가' })
+    @ApiBody({ type: CreateMeetingMemberDto })
+    @UseGuards(OptionalJwtAuthGuard)
     @Post(':meetingId/member')
     createMember(
         @Req() req,
@@ -52,32 +56,32 @@ export class MeetingsController {
     }
 
     @ApiBearerAuth()
-    @ApiOperation({ summary: '모임 리스트 보기' })
+    @ApiOperation({ summary: '미팅 리스트 보기' })
     @UseGuards(JwtAuthGuard)
     @Get('list')
-    findMeetingsList(@Req() req) {
-        return this.meetingsService.findMeetingsList(req.user.id);
+    getMeetings(@Req() req) {
+        return this.meetingsService.getMeetings(req.user.id);
     }
 
-    @ApiOperation({ summary: '모임 스케줄 보기 ' })
+    @ApiOperation({ summary: '미팅 스케줄 보기' })
     @Get(':meetingId/schedule')
     getSchedules(@Param('meetingId') meetingId: number) {
         return this.meetingsService.getSchedules(meetingId);
     }
 
-    @ApiOperation({ summary: '' })
+    @ApiOperation({ summary: '미팅 내 멤버 목록 반환' })
     @Get(':meetingId/member')
     getMembers(@Param('meetingId') meetingId: number) {
         return this.meetingsService.getMembers(meetingId);
     }
 
-    @ApiOperation({ summary: '' })
+    @ApiOperation({ summary: '미팅 장소 결과 반환' })
     @Get(':meetingId/place')
     getPlace(@Param('meetingId') meetingId: number) {
         return this.meetingsService.getPlace(meetingId);
     }
 
-    @ApiOperation({ summary: '미팅내 닉네임 중복 체크' })
+    @ApiOperation({ summary: '미팅 내 닉네임 중복 체크' })
     @Get(':meetingId/:nickname')
     checkOverlapNickname(
         @Param('meetingId') meetingId: number,
@@ -86,22 +90,31 @@ export class MeetingsController {
         return this.meetingsService.checkOverlapNickname(meetingId, nickname);
     }
 
-    @ApiOperation({ summary: '' })
+    @ApiBearerAuth()
+    @ApiOperation({ summary: '미팅 업데이트' })
+    @ApiBody({ type: UpdateMeetingDto })
+    @UseGuards(JwtAuthGuard)
     @Put(':meetingId')
     update(
         @Param('meetingId') meetingId: number,
+        @Req() req,
         @Body() updateMeetingDto: UpdateMeetingDto
     ) {
-        return this.meetingsService.update(meetingId, updateMeetingDto);
+        return this.meetingsService.update(
+            meetingId,
+            req.user.id,
+            updateMeetingDto
+        );
     }
 
     @ApiOperation({ summary: '미팅 스케줄 수정' })
-    @UseGuards(JwtAuthGuard)
+    @ApiBody({ type: UpdateMeetingScheduleDto })
+    @UseGuards(OptionalJwtAuthGuard)
     @Put('schedule/:memberId')
     updateSchedule(
         @Req() req,
         @Param('memberId') memberId: number,
-        @Body() updateMeetingScheduleDto
+        @Body() updateMeetingScheduleDto: UpdateMeetingScheduleDto
     ) {
         return this.meetingsService.updateSchedule(
             memberId,
@@ -110,12 +123,19 @@ export class MeetingsController {
         );
     }
 
-    @ApiOperation({ summary: '' })
+    @ApiBearerAuth()
+    @ApiOperation({ summary: '미팅 멤버 삭제' })
+    @UseGuards(JwtAuthGuard)
     @Delete(':meetingId/member/:memberId')
     removeMember(
+        @Req() req,
         @Param('meetingId') meetingId: number,
         @Param('memberId') memberId: number
     ) {
-        return this.meetingsService.removeMember(meetingId, memberId);
+        return this.meetingsService.removeMember(
+            meetingId,
+            req.user.id,
+            memberId
+        );
     }
 }
