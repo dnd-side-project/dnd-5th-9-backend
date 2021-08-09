355eb66a3e0d3c0c7d88125661184138f16ffb12
Yihyun <hihyun16@gmail.com>
2021-08-10 00:27:23 +0900

    refactor: Add dto description - API 문서에 설명이 포함되지 않은 dto 수정

--- src/meetings/dto/create-meeting-member.dto.ts
+++ src/meetings/dto/create-meeting-member.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateMeetingMemberDto {
+    @ApiProperty({
+        example: '이현이현',
+        description: '멤버의 닉네임',
+    })
     @IsString()
     nickname: string;
 }
--- src/meetings/dto/update-meeting-schedule.dto.ts
+++ src/meetings/dto/update-meeting-schedule.dto.ts
@@ -1,7 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsArray } from 'class-validator';
 import MeetingSchedules from '../../entities/MeetingSchedules';
 
 export class UpdateMeetingScheduleDto {
+    @ApiProperty({
+        example: [
+            { startDate: 12345, endDate: 12347 },
+            { startDate: 12370, endDate: 12380 },
+        ],
+        description: '숫자로 나타낸 숫자 배열',
+    })
     @IsArray()
     schedules: MeetingSchedules[];
 }
